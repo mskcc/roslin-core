e44fb0e009959443790811f480264f1ff4e97e71
Nikhil <nikhilkumar516@gmail.com>
2019-04-22 13:31:30 -0400

    Ability to handle newer roslin done script

--- bin/roslin_analysis.py
+++ bin/roslin_analysis.py
@@ -18,40 +18,32 @@ import redis
 
 logger = logging.getLogger("roslin_analysis")
 logger.setLevel(logging.INFO)
-log_file_handler = logging.FileHandler('roslin_analysis.log')
-log_file_handler.setLevel(logging.INFO)
-log_formatter = logging.Formatter('%(asctime)s - %(message)s')
-log_file_handler.setFormatter(log_formatter)
-logger.addHandler(log_file_handler)
-
 redis_host = os.environ.get("ROSLIN_REDIS_HOST")
 core_bin_path = os.environ.get("ROSLIN_CORE_BIN_PATH")
 redis_port = int(os.environ.get("ROSLIN_REDIS_PORT"))
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
+roslin_done_path = "/ifs/work/pi/roslin-internal-services/roslin-done.py"
 
-def publish_jira_update_to_redis(output_directory,project_name,portal_status):
-    # connect to redis
-    logger.info('------- Sending JIRA update to Redis -------')
-    logger.info('project name: ' + project_name)
-    logger.info('output directory: ' + output_directory)
-    logger.info('portal status: ' + str(portal_status))
-    logger.info('core bin path: '+ core_bin_path)
-    data = {}
-    data['output_directory'] = output_directory
-    data['core_bin_path'] = core_bin_path
-    data['portal_status'] = str(portal_status)
-    data['project_name'] = project_name
-    redis_client.publish('roslin-done', json.dumps(data))
+def run_popen(name,command,log_stdout,log_stderr,shell):
+    logger.info('---------- Running '+ name + ' ----------')
+    logger.info('Args: '+ ' '.join(command))
+    single_process = Popen(command, stdout=log_stdout,stderr=log_stderr, shell=shell)
+    output = None
+    error = None
+    errorcode = None
+    output, error = single_process.communicate()
+    errorcode = single_process.returncode
+    if errorcode != 0:
+        logger.info(name + ' passed')
+    else:
+        logger.info(name + ' failed')
+    if output:
+        logger.info("----- "+name+" stdout -----\n" + str(output))
+    if error:
+        logger.info("----- "+name+" stderr -----\n" + str(error))
 
-def publish_mercurial_update_request_to_redis(project_name):
-    logger.info('------- Sending Mercurial update request to Redis -------')
-    logger.info('project name: ' + project_name)
-    data = dict()
-    data['project_name'] = project_name
-    redis_client.publish('roslin-mercurial-update', json.dumps(data))
-
-def run_command(params,pipeline_script_path,output_directory,project_name):
-    script_name = 'roslin_analysis_helper.py'    
+def run_analysis_helper(params,pipeline_script_path,output_directory,project_name):
+    script_name = 'roslin_analysis_helper.py'
     pipeline_script = os.path.join(pipeline_script_path,script_name)
     params_dict = params
     command_args = []
@@ -69,13 +61,27 @@ def run_command(params,pipeline_script_path,output_directory,project_name):
 		if single_arg_value == True:
 			command_args.append(str(single_arg))
     command = ['python',pipeline_script] + command_args
-    logger.info('---------- Running analysis helper ----------')
-    logger.info('Script path: ' + pipeline_script)
-    logger.info('Args: '+ ' '.join(command))
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-    proc.communicate()
-    exit_code = proc.wait()
-    return exit_code
+    run_popen("analysis helper",command,subprocess.PIPE,subprocess.PIPE,False)
+
+def run_roslin_done(project_results_dir,project_id,log_path,disable_portal,disable_jira,enable_slack):
+    project_uuid_search = re.search("[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}",project_results_dir)
+    project_uuid = None
+    if not project_uuid_search:
+        logger.error("Project uuid not found")
+        sys.exit(1)
+    else:
+        project_uuid = project_uuid_search.group(0)
+    if not os.path.exists(roslin_done_path):
+        logger.warning(roslin_done_path + " not found. skipping")
+    roslin_done_command = ['python',roslin_done_path]
+    if disable_portal:
+        roslin_done_command.append('--disable_portal')
+    if disable_jira:
+        roslin_done_command.append('--disable_jira')
+    if not enable_slack:
+        roslin_done_command.append('--disable_slack')
+    roslin_done_command.extend(['--project_results_dir',project_results_dir,'--project_id',project_id,'--job_uuid',project_uuid,'--log_path',log_path])
+    run_popen("roslin done",roslin_done_command,subprocess.PIPE,subprocess.PIPE,False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="convert current project files to yaml input")
@@ -84,11 +90,20 @@ if __name__ == "__main__":
     parser.add_argument('--project_name',required=True,help='The name of the project')
     parser.add_argument('--disable_jira_update',default=False,action='store_true',help='If not updating JIRA, skips portion that comments to JIRA ticket associated with project.')
     parser.add_argument('--disable_portal_repo_update', default=False, action='store_true', help='If not updating cbioportal, skips submitting request to update the Mercurial repo.')
+    parser.add_argument('--enable_slack_update', default=False, action='store_true', help='Send project status info to slack')
     params = parser.parse_args()
     disable_jira_update = params.disable_jira_update
     disable_portal_repo_update = params.disable_portal_repo_update
+    enable_slack_update = params.enable_slack_update
+    disable_slack_update = True
     analysis_helper_args = {}
     output_directory = params.copy_outputs_directory
+    log_path = os.path.join(output_directory,'log','roslin_analysis.log')
+    log_file_handler = logging.FileHandler(log_path)
+    log_file_handler.setLevel(logging.INFO)
+    log_formatter = logging.Formatter('%(asctime)s - %(message)s')
+    log_file_handler.setFormatter(log_formatter)
+    logger.addHandler(log_file_handler)
     if not os.path.exists(output_directory):
         error_string = "The copy outputs directory does not exist"
         logger.error(error_string)
@@ -113,15 +128,6 @@ if __name__ == "__main__":
     analysis_helper_args['facets_directory'] = os.path.join(output_directory,'facets')
     analysis_helper_args['script_path'] = params.pipeline_bin_path
     analysis_helper_args['disable_portal_repo_update'] = params.disable_portal_repo_update
-   
-    exit_code = run_command(analysis_helper_args,params.pipeline_bin_path,output_directory,project_name)
 
-    if not disable_jira_update:
-        if exit_code == 0:
-            publish_jira_update_to_redis(output_directory,project_name,0)
-        else:
-            publish_jira_update_to_redis(output_directory,project_name,1)
-            disable_portal_repo_update = True
-
-    if not disable_portal_repo_update:
-         publish_mercurial_update_request_to_redis(project_name)
+    run_analysis_helper(analysis_helper_args,params.pipeline_bin_path,output_directory,project_name)
+    run_roslin_done(output_directory,project_name,log_path,disable_portal_repo_update,disable_jira_update,enable_slack_update)
